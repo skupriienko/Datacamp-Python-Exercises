9101bd300d1f83ab9a65c70f5cf41d71bbaef09d
skromnitsky <skromnitsky@gmail.com>
2020-11-10 18:53:07 +0200

    add two files

---
+++ Introduction to Statistics in Python/More Distributions and the Central Limit Theorem/1_Distribution of Amir's sales.py
@@ -0,0 +1,4 @@
+# Histogram of amount with 10 bins and show plot
+amir_deals['amount'].hist(bins=10)
+plt.show()
+
---
+++ Introduction to Statistics in Python/Random Numbers and Probability/3_Creating a probability distribution.py
@@ -0,0 +1,20 @@
+# Create a histogram of restaurant_groups and show plot
+restaurant_groups['group_size'].hist(bins=[2, 3, 4, 5, 6])
+plt.show()s
+
+
+# Create probability distribution
+size_dist = restaurant_groups['group_size'].value_counts() / restaurant_groups.shape[0]
+# Reset index and rename columns
+size_dist = size_dist.reset_index()
+size_dist.columns = ['group_size', 'prob']
+
+# Expected value
+expected_value = np.sum(size_dist['group_size'] * size_dist['prob'])
+
+# Subset groups of size 4 or more
+groups_4_or_more = size_dist[size_dist['group_size'] >= 4]
+
+# Sum the probabilities of groups_4_or_more
+prob_4_or_more = np.sum(groups_4_or_more['prob'])
+print(prob_4_or_more)
